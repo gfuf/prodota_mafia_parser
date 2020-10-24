a5e082832c37643a62cb96c7c6a77447325c0136
Denis Doshik <vi4096@gmail.com>
2020-10-25 00:07:49 +0300

    [v 0.2] add tests

--- src/main/java/prodota/data/content/SectionContent.java
+++ src/main/java/prodota/data/content/SectionContent.java
@@ -1,15 +1,31 @@
 package prodota.data.content;
 
+import org.apache.commons.collections4.CollectionUtils;
 import prodota.data.Section;
 import prodota.data.Topic;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public record SectionContent (Collection<Section> sections, Collection<Topic> topics)
 {
     public static SectionContent of(Collection<Section> sections, Collection<Topic> topics)
     {
         return new SectionContent(sections, topics);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SectionContent content = (SectionContent) o;
+        return CollectionUtils.isEqualCollection(sections, content.sections) &&
+                CollectionUtils.isEqualCollection(topics, content.topics);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
     }
 }
